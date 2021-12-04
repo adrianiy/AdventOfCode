25e2914420ecea6a4a3e6d5a55afbb26385cc8b4
Adrián Insua Yañez <adrianiy@ext.inditex.com>
2021-12-04 18:17:43 +0100

    feat: add day4 test

---
+++ src/21/day4/day4.test.ts
@@ -0,0 +1,21 @@
+import { bingo, bingoForLosers } from './day4';
+
+test('day4 test bingo!', () => {
+    const input = './day4/input_test.txt';
+    const result = bingo(input);
+    const loser = bingoForLosers(input);
+
+    expect(result).toEqual(4512);
+    expect(loser).toEqual(1924);
+})
+
+test('day3 results', () => {
+    const input = './day4/input.txt';
+    const result = bingo(input);
+    const loser = bingoForLosers(input);
+
+    //console.log(result);
+    //console.log(loser);
+    expect(result).toBeDefined();
+    expect(loser).toBeDefined();
+})
---
+++ src/21/day4/day4.ts
@@ -0,0 +1,88 @@
+import { loadFile } from "../utils";
+
+interface Winner {
+    board: string[][];
+    number: number;
+}
+
+const getBoards = (boardsRaw: string[]): string[][][] => {
+    const boards = [];
+    while (boardsRaw.length) {
+        boards.push(
+            boardsRaw.splice(0, 5)
+                .map(row =>
+                    row
+                        .split(' ')
+                        .filter(el => el.length)
+                )
+        );
+    }
+
+    return boards;
+}
+
+const checkWinner = (board: string[][], row: number, column: number): boolean => {
+    const matches: number[] = Array(5).fill(0).reduce((acc, _, index) => {
+        acc[0] += board[row][index].includes('_');
+        acc[1] += board[index][column].includes('_');
+
+        return acc;
+    }, [0, 0]);
+
+    return matches.some(direction => direction === 5);
+}
+
+const parseBoard = (board: string[][], number: number): boolean => {
+    return board.some((row, index) => {
+        return row.some((element, column) => {
+            if (+element === number) {
+                board[index][column] = `_${element}`;
+
+                return checkWinner(board, index, column);
+            }
+        })
+    });
+}
+
+const getPrize = (board: string[][], number: number): number => {
+    const nonMatching = board
+        .flat()
+        .reduce((acc, curr) => acc + (+curr || 0), 0);
+    return nonMatching * +number;
+}
+
+export const bingo = (input: string): number => {
+    const file = loadFile(input);
+    const numbers = file[0].split(',');
+    const boards = getBoards(file.slice(1).filter(row => row.length));
+
+
+    for (const number of numbers) {
+        for (const board of boards) {
+            if (parseBoard(board, +number)) {
+                return getPrize(board, +number);
+            }
+        }
+    }
+
+    return 0;
+}
+
+export const bingoForLosers = (input: string): number => {
+    const file = loadFile(input);
+    const numbers = file[0].split(',');
+    const boards = getBoards(file.slice(1).filter(row => row.length));
+
+    let winnerValue = 0;
+
+    for (const number of numbers) {
+        boards.forEach((board, index: number) => {
+            if (parseBoard(board, +number)) {
+                winnerValue = getPrize(board, +number);
+                boards[index] = [];
+            }
+        })
+    }
+
+    return winnerValue;
+}
